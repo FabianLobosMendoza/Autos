a1de091da947f4c197174cde920d6dde00019157
Pucciarellifabian <pucciarelli.fabian@partender.com>
2025-12-17 14:09:16 -0300

    Add noindex protections and robots directives

--- concesionario_project/settings.py
+++ concesionario_project/settings.py
@@ -40,6 +40,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'apps.core.middleware.NoIndexMiddleware',
 ]
 
 ROOT_URLCONF = 'concesionario_project.urls'
